32d373e2279258aff997fb3cf20fa931bd3524b8
David O Neill <dmz.oneill@gmail.com>
2018-03-14 22:15:05 +0000

    Updates for coveralls.io

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-mkdir -vp ~/.config/jaggedsoft/
-
-cat > ~/.config/jaggedsoft/php-binance-api.json <<EOT
-{
-    "api-key": "z5RQZ9n8JcS3HLDQmPpfLQIGGQN6TTs5pCP5CTnn4nYk2ImFcew49v4ZrmP3MGl5",
-    "api-secret": "ZqePF1DcLb6Oa0CfcLWH0Tva59y8qBBIqu789JEY27jq0RkOKXpNl9992By1PN9Z"
-}
-EOT
-
 composer install
-./vendor/bin/phpunit --verbose --debug --bootstrap vendor/autoload.php BinanceTest
+./vendor/bin/phpunit --verbose --debug --coverage-clover build/logs/clover.xml --bootstrap vendor/autoload.php BinanceTest
+php ./vendor/bin/coveralls -v
