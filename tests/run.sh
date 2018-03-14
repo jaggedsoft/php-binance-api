cdfbdc51f78bcf1d263570a2adfc5fe86e4b2465
David O Neill <dmz.oneill@gmail.com>
2018-03-14 21:22:41 +0000

    adding api and secret key for test automation

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cat > ~/.config/jaggedsoft/php-binance-api.json <<EOT
+{
+    "api-key": "z5RQZ9n8JcS3HLDQmPpfLQIGGQN6TTs5pCP5CTnn4nYk2ImFcew49v4ZrmP3MGl5",
+    "api-secret": "ZqePF1DcLb6Oa0CfcLWH0Tva59y8qBBIqu789JEY27jq0RkOKXpNl9992By1PN9Z"
+}
+EOT
 
 composer install
 ./vendor/bin/phpunit --verbose --debug --bootstrap vendor/autoload.php BinanceTest
