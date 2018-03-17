371771f9732fb87958aefd1b42264214bec5c93f
Dave <dmz.oneill@gmail.com>
2018-03-17 16:21:05 +0000

    Update prep.sh

--- tests/prep.sh
+++ tests/prep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PHP_UNIT_DIR=$(pwd)
 mkdir -vp ~/.config/jaggedsoft/
 
 cat > ~/.config/jaggedsoft/php-binance-api.json <<EOT
@@ -9,9 +10,35 @@ cat > ~/.config/jaggedsoft/php-binance-api.json <<EOT
 }
 EOT
 
+cat > ./phpunit.xml <<EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<phpunit
+        backupGlobals="true"
+        colors="true"
+        stopOnFailure="false">
+
+    <filter>
+        <whitelist>
+            <directory>$PHP_UNIT_DIR/../</directory>
+            <exclude>
+                <directory>$PHP_UNIT_DIR/../tests</directory>
+                <directory>$PHP_UNIT_DIR/vendor</directory>
+            </exclude>
+        </whitelist>
+    </filter>
+
+    <testsuite name="PHP Binance Api">
+        <directory suffix=".php">../</directory>
+    </testsuite>
+
+</phpunit>
+EOT
+
 mkdir -p build/logs
 
 travis_retry composer install --no-interaction --no-suggest
 wget -c -nc --retry-connrefused --tries=0 https://github.com/php-coveralls/php-coveralls/releases/download/v2.0.0/php-coveralls.phar -O coveralls.phar
 chmod +x coveralls.phar
 php coveralls.phar --version
+
+sleep 5
