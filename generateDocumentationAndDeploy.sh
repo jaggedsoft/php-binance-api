5d86d627f72a572ef8f1d1d8d5b398b463de0601
dmzoneill <dmz.oneill@gmail.com>
2018-03-27 15:01:40 +0100

    Updates for documentation generation

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -79,7 +79,7 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 # Only upload if Doxygen successfully created the documentation.
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+if [ -d "doc/html" ] && [ -f "doc/html/index.html" ]; then
 
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
