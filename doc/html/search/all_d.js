4a7ff17d449938d18b68dd7c24f9826a43d5f969
Travis CI <travis@travis-ci.org>
2018-03-27 16:19:58 +0000

    Deploy code docs to GitHub Pages Travis build: 92
    Commit: 658d2ae1955d13d7a179a1d72322c485c953e823

--- doc/html/search/all_d.js
+++ doc/html/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['openorders',['openOrders',['../classBinance_1_1API.html#ad061d80912249564555a144ba5274ea6',1,'Binance::API']]],
+  ['openorders',['openOrders',['../classBinance_1_1API.html#aaf2a8365d8f8a4a336c422183cddd0bf',1,'Binance::API']]],
   ['order',['order',['../classBinance_1_1API.html#a672f348fa579428bc57e7f3673214c0c',1,'Binance::API']]],
   ['orders',['orders',['../classBinance_1_1API.html#a7d7f83f4843ad13d1ee812a9ca08facb',1,'Binance::API']]],
   ['orderstatus',['orderStatus',['../classBinance_1_1API.html#adc33d167e8c8fa8aba1831de948b1558',1,'Binance::API']]]
