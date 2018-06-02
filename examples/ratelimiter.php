970d0924778cd94f25755eabb0c648f909ec6655
David O Neill <dmz.oneill@gmail.com>
2018-06-02 20:51:40 +0100

    Rate Limiting

--- examples/ratelimiter.php
+++ examples/ratelimiter.php
@@ -1,12 +1,17 @@
 <?php
 
 require '../php-binance-api.php';
+require '../php-binance-api-rate-limiter.php';
 
-// @see home_directory_config.php
 // use config from ~/.confg/jaggedsoft/php-binance-api.json
 $api = new Binance\API();
+$api = new Binance\RateLimiter($api);
 
 // Get latest price of a symbol
 $ticker = $api->prices();
-print_r($ticker); // List prices of all symbols
 echo "Price of BNB: {$ticker['BNBBTC']} BTC.\n";
+
+while(true)
+{
+    $api->openOrders("BNBBTC");
+}
