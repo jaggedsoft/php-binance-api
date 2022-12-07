a960691dbb488e0b19470933d25e91717573c6ad
Jérôme Farin <39909515+JeromeFarin@users.noreply.github.com>
2022-12-07 20:31:42 +0000

    fix: get filter by "LOT_SIZE"

--- php-binance-api.php
+++ php-binance-api.php
@@ -410,7 +410,14 @@ class API
     public function marketQuoteSell(string $symbol, $quantity, array $flags = [])
     {
         $flags['isQuoteOrder'] = true;
-        $c = $this->numberOfDecimals($this->exchangeInfo()['symbols'][$symbol]['filters'][2]['minQty']);
+        $exchangeInfo = $this->exchangeInfo();
+        $filters = $exchangeInfo['symbols'][$symbol]['filters'];
+        $lotSize = array_filter($filters, function ($filter) {
+            return $filter['filterType'] === 'LOT_SIZE';
+        });
+        $lotSize = array_shift($lotSize);
+        $minQty = $lotSize['minQty'];
+        $c = $this->numberOfDecimals($minQty);
         $quantity = $this->floorDecimal($quantity, $c);
 
         return $this->order("SELL", $symbol, $quantity, 0, "MARKET", $flags);
@@ -446,7 +453,14 @@ class API
      */
     public function marketSell(string $symbol, $quantity, array $flags = [])
     {
-        $c = $this->numberOfDecimals($this->exchangeInfo()['symbols'][$symbol]['filters'][2]['minQty']);
+        $exchangeInfo = $this->exchangeInfo();
+        $filters = $exchangeInfo['symbols'][$symbol]['filters'];
+        $lotSize = array_filter($filters, function ($filter) {
+            return $filter['filterType'] === 'LOT_SIZE';
+        });
+        $lotSize = array_shift($lotSize);
+        $minQty = $lotSize['minQty'];
+        $c = $this->numberOfDecimals($minQty);
         $quantity = $this->floorDecimal($quantity, $c);
 
         return $this->order("SELL", $symbol, $quantity, 0, "MARKET", $flags);
